9c79e18f8479dd098cda52a2bbb9e21c52360e0c
Sein Tun <contact@seintun.dev>
2024-07-27 23:08:30 -0700

    Add Post class

--- network/models.py
+++ network/models.py
@@ -1,6 +1,17 @@
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author")
+    content = models.TextField(max_length=120)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"#{self.id}: {self.user} created #{self.id} {str(self.content)[:25]} on ({str(self.timestamp)})"
